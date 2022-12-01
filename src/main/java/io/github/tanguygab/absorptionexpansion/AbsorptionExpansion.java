94c19b4f7612c8f216dd9d693f0996ddd6ceed9d
Tanguygab <tanguy@gabril.com>
2022-12-01 19:49:51 +0100

    Initial commit

---
+++ src/main/java/io/github/tanguygab/absorptionexpansion/AbsorptionExpansion.java
@@ -0,0 +1,55 @@
+package io.github.tanguygab.absorptionexpansion;
+
+import me.clip.placeholderapi.expansion.PlaceholderExpansion;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public final class AbsorptionExpansion extends PlaceholderExpansion {
+
+    private Method getAbsorption;
+
+    public AbsorptionExpansion() {
+        try {
+            Class<?> entityPlayerClass = Class.forName("net.minecraft.server."+Bukkit.getServer().getClass().getPackage().getName().split("\\.")[3]+".EntityPlayer");
+            getAbsorption = entityPlayerClass.getMethod("getAbsorptionHearts");
+        } catch (Exception e) {}
+    }
+
+    @Override
+    public String getIdentifier() {
+        return "absorption";
+    }
+
+    @Override
+    public String getAuthor() {
+        return "Tanguygab";
+    }
+
+    @Override
+    public String getVersion() {
+        return "1.0.0";
+    }
+
+    @Override
+    public List<String> getPlaceholders() {
+        return Arrays.asList("%absorption_absorption%","%absorption_health%");
+    }
+
+    @Override
+    public String onPlaceholderRequest(Player p, String params) {
+        if (p == null) return "";
+        if (params.equalsIgnoreCase("absorption")) return getAbsorption(p)+"";
+        if (params.equalsIgnoreCase("health")) return (p.getHealth()+getAbsorption(p))+"";
+        return null;
+    }
+
+    public float getAbsorption(Player p) {
+        try {return (float) getAbsorption.invoke(p);}
+        catch (Exception e) {return 0;}
+    }
+}
