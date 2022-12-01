ee57b8aeb185f249d1d88d893dcb453a8e582123
Tanguygab <tanguy@gabril.com>
2022-12-01 19:56:15 +0100

    Initial Commit

--- src/main/java/io/github/tanguygab/absorptionexpansion/AbsorptionExpansion.java
+++ src/main/java/io/github/tanguygab/absorptionexpansion/AbsorptionExpansion.java
@@ -6,7 +6,6 @@ import org.bukkit.entity.Player;
 
 import java.lang.reflect.Method;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 public final class AbsorptionExpansion extends PlaceholderExpansion {
